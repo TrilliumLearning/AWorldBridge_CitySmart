34f16f62f79c273e803bd62b9e14cbfe047a3945
Daniel <danieldrdj01@gmail.com>
2017-07-02 22:16:53 -0800

    Clean Up
    I cleaned up the directories and organized a little bit and i also created a new directory to test out the code that we will start making a new interface out of. The plan is to make it dynamically with information from a csv file.

--- CitySmart/Globe_C.js
+++ CitySmart/Globe_C.js
@@ -41,7 +41,7 @@ var i = 0;
 
 var Globe_myArray1 = [];
 
-var url = "http://cs.aworldbridgelabs.com/CitySmart/Layer_Files/Layer_csv/Master.csv";
+
 //var Globe_url = "http://localhost:63342/AWorldBridge_CitySmart/CitySmart/Layer_Files/Layer_csv/Master.csv";
 //var Globe_url = "http://10.194.40.100/wwdev/CitySmart/Layer_Files/Layer_csv/Master.csv";
 
