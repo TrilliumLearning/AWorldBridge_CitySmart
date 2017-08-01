d94a500263bb5ba706dce3ff6f0f2035891e60a6
kiae shin <kiemshin@outlook.com>
2017-08-01 00:50:38 -0800

    Update
    Completed ftaa graph pages

---
+++ CitySmart/graphsftaa.js
@@ -0,0 +1,115 @@
+$(document).ready(function() {
+
+      window.fun = function()
+{
+    var ddl = document.getElementById("dropDown");
+    var selectedValue = ddl.options[ddl.selectedIndex].value;
+      
+      
+    if (selectedValue == "Calcium")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/calcium.png';
+   }
+          
+      
+      if (selectedValue == "Ammonium")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/ammonium.png';
+       
+   }
+      
+      if (selectedValue == "Potassium")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/potassium.png';
+       
+   }
+      
+      if (selectedValue == "Chloride")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/chloroide.png';
+   }
+      
+       if (selectedValue == "Colorimeter")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       
+   }
+          
+      
+      if (selectedValue == "Turbidity")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/turbidity.png';
+       
+   }
+      
+      if (selectedValue == "pH")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/ph.png';
+   }
+      
+      if (selectedValue == "Temperature")
+   {
+       document.getElementById('textpic').innerHTML = "";
+       var img = new Image();
+	   var div = document.getElementById('textpic');
+
+	   img.onload = function() {
+  	   div.appendChild(img);
+		};
+
+	   img.src = 'water_experiment/temp.png';
+   }
+      
+      
+     
+    };
+    
+});
